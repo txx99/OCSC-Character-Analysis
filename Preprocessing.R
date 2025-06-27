1ef98741d9e37518af60204e4d83f0dabd096dec
Noha El-Haj <elhajn02@gmail.com>
2025-06-26 20:12:34 -0400

    preprocessing file - data exploration + edit na values in data import

--- Preprocessing.R
+++ Preprocessing.R
@@ -11,31 +11,31 @@ A778_D0 <- read_tsv(file = "Read_Count_Data/A778_D0_gene.tsv",
                     col_names = c("ensembl_gene_rec",
                                   "gene_name",
                                   "read_count"),
-                    na = c("0"))
+                    na = "0")
 A778_D6 <- read_tsv(file = "Read_Count_Data/A778_D6_gene.tsv", 
                     col_names = c("ensembl_gene_rec", 
                                   "gene_name", 
                                   "read_count"), 
-                    na = c("0"))
+                    na = "0")
 
 # patient 2
 A820_D0 <- read_tsv(file = "Read_Count_Data/A820_D0_gene.tsv", 
                     col_names = c("ensembl_gene_rec", 
                                   "gene_name", 
                                   "read_count"), 
-                    na = c("0"))
+                    na = "0")
 A820_D6 <- read_tsv(file = "Read_Count_Data/A820_D6_gene.tsv", 
                     col_names = c("ensembl_gene_rec", 
                                   "gene_name", 
                                   "read_count"), 
-                    na = c("0"))
+                    na = "0")
 
 # patient 3
 A870_D0 <- read_tsv(file = "Read_Count_Data/A870_D0_gene.tsv", 
                     col_names = c("ensembl_gene_rec", 
                                   "gene_name", 
                                   "read_count"), 
-                    na = c("0"))
+                    na = "0")
 A870_D6 <- read_tsv(file = "Read_Count_Data/A870_D6_gene.tsv", 
                     col_names = c("ensembl_gene_rec", 
                                   "gene_name", 
@@ -47,12 +47,12 @@ A899_D0 <- read_tsv(file = "Read_Count_Data/A899_D0_gene.tsv",
                     col_names = c("ensembl_gene_rec", 
                                   "gene_name", 
                                   "read_count"), 
-                    na = c("0"))
+                    na = "0")
 A899_D6 <- read_tsv(file = "Read_Count_Data/A899_D6_gene.tsv", 
                     col_names = c("ensembl_gene_rec", 
                                   "gene_name", 
                                   "read_count"), 
-                    na = c("0"))
+                    na = "0")
 
 # ----- create individual df for each patient -----
 A778_df <- merge(A778_D0, 
@@ -77,6 +77,25 @@ colnames(A899_df) <- c("ensembl_gene_rec", "gene_name", "d0_rc", "d6_rc")
 
 # create list of all patients for ease of function application
 patients_list <- mget(ls(pattern = "^A.*_df$"))
+
+# ----- data exploration -----
+lapply(patients_list, head)
+lapply(patients_list, summary) 
+lapply(patients_list, str)
+lapply(patients_list[[1]], typeof)
+lapply(patients_list[[1]], class)
+
+# check for OCSC genes in dataset
+relevant_genes <- list()
+for (i in 1:length(patients_list)) {
+  relevant_genes[[i]] <- patients_list[[i]] %>% filter(gene_name == "ALDH1A1"| 
+                                                         gene_name =="CD44"| 
+                                                         # gene_name =="CD133"| 
+                                                         gene_name == "CD24"| 
+                                                         # gene_name =="CD117"| 
+                                                         gene_name == "EPCAM"| 
+                                                         gene_name ==  "THY1")
+}
 
 
 
